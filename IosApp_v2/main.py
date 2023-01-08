3a33922338fa05613925d13d8ee5351a0ead11bf
“Pier <p.strbad0@gmail.com>
2023-01-08 12:26:15 +0100

    Poboljsano sucelje

--- IosApp_v2/main.py
+++ IosApp_v2/main.py
@@ -1,70 +1,113 @@
 import kivy
-kivy.require('2.1.0')
+kivy.require("2.1.0")
 
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.pickers import MDDatePicker
+from kivy.properties import DictProperty
+from kivy.core.window import Window
 
-class ScreenManagment(ScreenManager):
-    
-    def __init__(self, **kwargs):
-        super(ScreenManagment, self).__init__(**kwargs)
+##Print Bills##
+class PrintBillsScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
 
-class SecondScreen(Screen):
-    
-    def __init__(self, **kwargs):
-        super(SecondScreen, self).__init__(**kwargs)
-    
-    def main_screen(self):
-        self.manager.current = 'main'
+##Amount Sold##
+class AmountSoldScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
 
-class MainScreen(Screen):
+##Calendar##
+class CalendarScreen(MDScreen):
     
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-                    
-    def changeScreen(self, string):
+    def DatePicker(self):
+        self.dateD = MDDatePicker()
+        self.dateD.open()
         
-        artikli = ["Artikl: Kruh \nCijena: 8.00 kn \nKolicina: 20",
-                   "Artikl: Sladoled \nCijena: 20.00 kn \nKolicina: 30",
-                   "Artikl: Jaja \nCijena: 13.00 kn \nKolicina: 21",
-                   "Artikl: Majoneza \nCijena: 16.00 kn \nKolicina: 32",
-                   "Artikl: Jabuke \nCijena: 6.00 kn \nKolicina: 60",
-                   "Artikl: Nutella \nCijena: 22.00 kn \nKolicina: 30",
-                   "Artikl: Meso \nCijena: 40.00 kn \nKolicina: 18",
-                   "Artikl: Jogurt \nCijena: 20.00 kn \nKolicina: 15"]
-        
-        if string == "Kruh":
-            self.manager.get_screen('second').ids.lb1.text = artikli[0]
-            
-        elif string == "Sladoled":
-            self.manager.get_screen('second').ids.lb1.text = artikli[1]
-            
-        elif string == "Jaja":
-            self.manager.get_screen('second').ids.lb1.text = artikli[2]
-            
-        elif string == "Majoneza":
-            self.manager.get_screen('second').ids.lb1.text = artikli[3]
-            
-        elif string == "Jabuke":
-            self.manager.get_screen('second').ids.lb1.text = artikli[4]
-            
-        elif string == "Nutella":
-            self.manager.get_screen('second').ids.lb1.text = artikli[5]
-            
-        elif string == "Meso":
-            self.manager.get_screen('second').ids.lb1.text = artikli[6]
-            
-        elif string == "Jogurt":
-            self.manager.get_screen('second').ids.lb1.text = artikli[7]
-            
-        self.manager.current = 'second'
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
 
-class mainApp(App):
+##Store Location##
+class StoreLocationScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
+
+##Mail##
+class MailScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
+
+##Phonebook##
+class PhonebookScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
+
+##News##
+class NewsScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
+
+##Settings##
+class SettingsScreen(MDScreen):
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
+
+class MainScreen(MDScreen):
+    
+    def ChangeScreen(self, ScreenName):
+        self.manager.current = ScreenName
+                  
+class PerfectStoreApp(MDApp):
+
+    data = DictProperty()
+    Window.size = (375, 812)
+
     def build(self):
-        sm = ScreenManagment(transition = SlideTransition())
+        sm = MDScreenManager()
+        
+        self.data = {
+            'Store Location': [
+                'map-marker-outline',
+                'on_press', lambda x: callback('SL')
+            ],
+            'Mail':[
+                'email-outline',
+                'on_press', lambda x: callback('M')
+            ],
+            'Phonebook':[
+                'book-account',
+                'on_press', lambda x: callback('P')
+            ],
+            'News':[
+                'newspaper',
+                'on_press', lambda x: callback('N')
+            ],
+            'Settings':[
+                'cog-outline',
+                'on_press', lambda x: callback('S')
+            ]
+        }
+        
         sm.add_widget(MainScreen(name = 'main'))
-        sm.add_widget(SecondScreen(name = 'second'))
+        sm.add_widget(PrintBillsScreen(name = 'bill'))
+        sm.add_widget(AmountSoldScreen(name = 'amount'))
+        sm.add_widget(CalendarScreen(name = 'calendar'))
+        sm.add_widget(StoreLocationScreen(name = 'SL'))
+        sm.add_widget(MailScreen(name = 'M'))
+        sm.add_widget(PhonebookScreen(name = 'P'))
+        sm.add_widget(NewsScreen(name = 'N'))
+        sm.add_widget(SettingsScreen(name = 'S'))
+        
+        def callback(instance):
+            sm.get_screen('main').ids.speedDial.close_stack()
+            sm.current = instance
+        
         return sm
-
+    
+    def on_start(self):
+        self.fps_monitor_start()
+    
 if __name__ == '__main__':
-    mainApp().run()
+    PerfectStoreApp().run()
+        
